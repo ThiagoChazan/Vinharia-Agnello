0aa560fcb3a9d8703eb331af931fe1e870522a9e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-23 19:58:34 -0300

    Alterando o Console.log

--- src/script.js
+++ src/script.js
@@ -10,4 +10,7 @@ var numb4 = prompt('Digite a quantidade em estoque:');
 
 alert('Cadastro realizado! Veja os detalhes no console');
 
-console.log('Vinho:', numb, numb2, numb3, numb4);
+console.log('Vinho:', numb);
+console.log('Vinho:', numb2);
+console.log('Vinho:', numb3);
+console.log('Vinho:', numb4);
